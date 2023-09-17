fe3ace618b0e6510141d00e329d4f4d226ba500e
Adrián Flores <flo21500@uvg.edu.gt>
2023-09-17 03:02:53 -0600

    Revert "Initial commit"
    This reverts commit 021efd54eba91002b29b9bf635880b0e0dc3aad1.

--- Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	g++ -Isrc/include -Lsrc/lib -o run main.cpp -lmingw32 -lSDL2main -lSDL2 -fopenmp
--- camera.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct Camera {
-  glm::vec3 pos;
-  glm::vec3 target;
-  glm::vec3 up;
-};
